4573d2c3e68f66e5984a0ceaed6e5f418d124014
Gustavo-Zamai <gustavosimaozamai@gmail.com>
2023-07-15 23:05:01 -0300

    Increase script with pokemons details dynamic

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,20 @@
+function convertPokemonTypesToList(pokemonTypes) {
+  return  pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
+}
+
+
+
 function convertePokemonToList(pokemon) {
   return`
     <li class="pokemon">
-    <span class="number">#001</span>
+    <span class="number">#${pokemon.order}</span>
     <span class="name">${pokemon.name}</span>
     <div class="detail">
       <ol class="types">
-        <li class="type">grass</li>
-        <li class="type">poison</li>
+        ${convertPokemonTypesToList(pokemon.types).join('')}
       </ol>
       <img
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+        src="${pokemon.sprites.other.dream_world.front_default}"
         alt="${pokemon.name}"
       />
     </div>
@@ -20,7 +25,8 @@ function convertePokemonToList(pokemon) {
 const pokemonList = document.getElementById('pokemonList')
 
 pokeApi.getPokemons().then((pokemons = []) => { 
-    pokemonList.innerHTML += pokemons.map(convertePokemonToList).join('');
+  const newListHtml =  pokemons.map(convertePokemonToList).join('');
+  pokemonList.innerHTML = newListHtml
     //const listItens = [];
     //for (let i = 0; i < pokemons.length; i++) {
     //    const pokemon = pokemons[i];
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,10 +1,17 @@
 const pokeApi = {};
 
+pokeApi.getPokemonDetail = (pokemon) => {
+ return fetch(pokemon.url)
+          .then((response) => response.json()) 
+}
+
 pokeApi.getPokemons = (offset = 0, limit = 12) => {
   const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
   return fetch(url)
     .then((response) => response.json())
     .then((jsonBody) => jsonBody.results)
-    .catch((error) => console.log(error));
-};
+    .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+    .then((detailRequests) => Promise.all(detailRequests))
+    .then((pokemonsDetails) => pokemonsDetails)
+}
