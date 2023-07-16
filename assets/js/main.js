150614b91c99cc80e7c49ee61021a99ae7de4288
Gustavo-Zamai <gustavosimaozamai@gmail.com>
2023-07-16 15:56:00 -0300

    Add max number of records to according of first gen

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,17 +4,16 @@
 
 const pokemonList = document.getElementById("pokemonList");
 const loadMoreBtn = document.getElementById("loadMore");
+
+const maxNumRecords = 151;
 const limit = 12;
 let offset = 0;
 
-
-
-
-
-
 function loadPokemonItens(offset, limit) {
   pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
-    const newListHtml = pokemons.map((pokemon) => `
+    const newListHtml = pokemons
+      .map(
+        (pokemon) => `
       <li class="pokemon ${pokemon.type}">
       <span class="number">#${pokemon.number}</span>
       <span class="name">${pokemon.name}</span>
@@ -30,16 +29,29 @@ function loadPokemonItens(offset, limit) {
         />
       </div>
     </li>
-    `).join('')
-    pokemonList.innerHTML += newListHtml
-  })
+    `
+      )
+      .join("");
+    pokemonList.innerHTML += newListHtml;
+  });
 }
 
-loadPokemonItens(offset, limit)
+loadPokemonItens(offset, limit);
 
 loadMoreBtn.addEventListener("click", () => {
   offset += limit;
-  loadPokemonItens(offset, limit);
+  const numRecordsNextPage = offset + limit;
+
+  if (numRecordsNextPage >= maxNumRecords) {
+    const newLimit = maxNumRecords - offset;
+
+    loadPokemonItens(offset, newLimit);
+
+    loadMoreBtn.parentElement.removeChild(loadMore);
+    return;
+  } else {
+    loadPokemonItens(offset, limit);
+  }
 });
 
 //const listItens = [];
